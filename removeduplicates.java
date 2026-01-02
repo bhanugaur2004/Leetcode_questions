e5d76e32b235637739fc3538f01758d769e45a06
bhanu <bhanugaur2004@gmail.com>
2026-01-02 15:57:04 +0530

    Ho gaya

--- removeduplicates.java
+++ removeduplicates.java
@@ -1,18 +1,29 @@
-// import java.util.Scanner;
-
 public class removeduplicates {
     public static void main(String[] args) {
-        // Scanner sc = new Scanner(System.in);
-        int[] nums = { 1, 2, 3, 4, 5, 6, 7, 7, 8, 8 };
-        for (int i = 0; i < nums.length - 1; i++) {
-            for (int j = i + 1; j < nums.length - 1; j++) {
-                if (nums[i] == nums[j]) {
-                    System.out.println("There is an duplicate.");
-                    break;
-                }
+        int[] nums = { 0, 0, 1, 1, 1, 2, 2, 3, 3, 4 };
+
+        int k = removeDuplicates(nums);
+
+        System.out.println("k = " + k);
+        System.out.print("Unique elements: ");
+        for (int i = 0; i < k; i++) {
+            System.out.print(nums[i] + " ");
+        }
+    }
+
+    public static int removeDuplicates(int[] nums) {
+        if (nums.length == 0)
+            return 0;
+
+        int k = 0;
+
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] != nums[k]) {
+                k++;
+                nums[k] = nums[i];
             }
         }
-        // System.out.println("Woho!!!!");
-        // sc.close();
+
+        return k + 1;
     }
 }
